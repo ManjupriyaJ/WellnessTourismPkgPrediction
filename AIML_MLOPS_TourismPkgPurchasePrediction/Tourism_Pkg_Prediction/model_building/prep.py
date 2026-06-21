502f5ad58c48620f4b594445200d3945544d7584
sasirekhad <sasirekha.duraibabu@gmail.com>
2026-06-21 11:03:31 +0000

    New HF Space Change Cleanup

--- AIML_MLOPS_TourismPkgPurchasePrediction/Tourism_Pkg_Prediction/model_building/prep.py
+++ AIML_MLOPS_TourismPkgPurchasePrediction/Tourism_Pkg_Prediction/model_building/prep.py
@@ -10,7 +10,7 @@ from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
-DATASET_PATH = "hf://datasets/ManjupriyaJ/Tourism-Package-Purchase-Prediction/tourism.csv"
+DATASET_PATH = "hf://datasets/Sasimscct/Tourism-Package-Purchase-Prediction/tourism.csv"
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
